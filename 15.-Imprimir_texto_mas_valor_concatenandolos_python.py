b21675060ab742a71089854faf7872d18e997e10
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 20:20:52 -0600

    Add files via upload

---
+++ 15.-Imprimir_texto_mas_valor_concatenandolos_python.py
@@ -0,0 +1,12 @@
+# Este programa muestra como concatenar una texto con un valor
+
+print("Este programa muestra como concatenar una texto con un valor")
+print("Para hacerlo cerramos comillas, separamos con una coma y colocamos la variable")
+print("Antes de cerrar el paréntesis")
+numero1 = 34.12
+print("El valor del numero es: ", numero1)
+print("La variable es del tipo: ")
+print(type(numero1))
+print("Este programa fue escrito por Emilio Carcaño Bringas")
+
+# Este programa fue escrito por Emilio Carcaño Bringas
